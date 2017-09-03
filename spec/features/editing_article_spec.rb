8cc3d394f290e8c788d9abe3c6f8f6b72e14a6d6
ivane gomarteli <ivanejg@yahoo.com>
2017-09-03 17:39:27 +0300

    fix brokn editing article spec

--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.feature "Editing an article" do 
 
 	before do
-		@article = Article.create(title: "The first article", body: "Lorem ipsum 1st.")
+		john = User.create(email: "john@email.com", password: "password")
+		login_as(john)
+		@article = Article.create(title: "The first article", body: "Lorem ipsum 1st.", user: john)
 	end
 
 	scenario "A user updates an article" do 
