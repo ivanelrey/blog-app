762d83de2ca4718f640e76ef2d7c37a80bc47dcb
ivane gomarteli <ivanejg@yahoo.com>
2017-09-06 22:08:42 +0300

    add real time comments feature

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,12 +9,14 @@ class CommentsController < ApplicationController
 			@comment = @article.comments.build(comment_params)
 			@comment.user = current_user
 
-			if @comment.save 
+			if @comment.save
+				ActionCable.server.broadcast "comments", 
+						render(partial: 'comments/comment', object: @comment)
 				flash[:notice] = 'Comment has been created'
 			else
 				flash.now[:alert] = 'Comment has not been created'
+				redirect_to article_path(@article)
 			end
-			redirect_to article_path(@article)
 		end
 	end
 	private
